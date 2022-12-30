b266d2f651adecc7dd47722d5ddf589a1f21e1b4
Andriychuk <ANDRIN07@pfizer.com>
2022-12-30 16:11:28 -0500

    update chart theme and labels

--- R/AEplot.R
+++ R/AEplot.R
@@ -26,9 +26,9 @@ AEplot <- function(data, paramVar, aeStartVar, aeEndVar, colorVar, ...) {
     geom_segment(aes(x={{aeStartVar}}, xend={{aeEndVar}}, y={{paramVar}}, yend={{paramVar}}, color={{colorVar}}), linetype=1, size=2) +
     scale_colour_brewer(palette = "Pastel1")+
     xlab("Study Day Start/End of AE")+
-    ylab("AE Preferred Term") +
-    scale_x_continuous(limits=c(0,max(na.omit(data$AENDY)))) #+
-    # theme_bw() + theme(panel.grid.minor = element_blank(), panel.grid.major =   element_blank()) + theme(aspect.ratio = .2)
+    ylab("") +
+    scale_x_continuous(limits=c(0,max(na.omit(data$AENDY)))) +
+    theme_bw()
   p + theme(legend.position="none")
 
   return(p)
