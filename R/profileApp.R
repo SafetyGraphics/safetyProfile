841fc5856b8dc8395473dfb1562c46768a244db7
Spencer Childress <spencer.c.childress@gmail.com>
2023-02-23 10:01:25 -0500

    idk

--- R/profileApp.R
+++ R/profileApp.R
@@ -12,7 +12,11 @@
 #' @export
 
 profileApp <- function(
-    data = NULL,
+    data = list(
+        dm = safetyData::adam_adsl,
+        aes = safetyData::adam_adae,
+        labs = safetyData::adam_adlbc
+    ),
     settings = NULL,
     standard = 'adam',
     runNow = TRUE
@@ -26,40 +30,12 @@ profileApp <- function(
 
     standard <- tolower(standard)
 
-    ## Create list of default data when undefined.
+    # TODO: use default data given standard
     if (is.null(data))
-        data <- getDefaultData(standard)
+        settings <- safetyProfile::example_data[[ standard ]]
 
-    ## Create list of default settings when undefined.
     if (is.null(settings))
-        settings <- getDefaultSettings(standard)
-
-    ## Calculate study timing.
-    if (standard == 'sdtm') {
-        data$aes <- getTiming(
-            params = list(
-                data = data,
-                settings = settings,
-                standard = standard
-            ),
-            'aes',
-            domainStartDate = settings$aes$stdt_col,
-            domainEndDate = settings$aes$endt_col
-        )
-        settings$aes$stdy_col <- paste0(settings$aes$stdt_col, '_dy')
-        settings$aes$endy_col <- paste0(settings$aes$endt_col, '_dy')
-
-        data$labs <- getTiming(
-            params = list(
-                data = data,
-                settings = settings,
-                standard = standard
-            ),
-            'labs',
-            domainDate = settings$labs$dt_col
-        )
-        settings$labs$dy_col <- paste0(settings$labs$dt_col, '_dy')
-    }
+        settings <- safetyProfile::mapping[[ standard ]]
 
     ## Create reactive list of data and settings to pass to server
     params <- reactive({
